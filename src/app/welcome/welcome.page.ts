fde2eeaf9c4a0fc01c9d94e35749649541fbd2a3
Seyed Mohammad Ayyoubzadeh <s.m.ayyoubzadeh@gmail.com>
2021-10-20 23:28:29 +0330

    Co-authored-by: Seyed Moein Ayyoubzadeh <smoeina@users.noreply.github.com>

--- src/app/welcome/welcome.page.ts
+++ src/app/welcome/welcome.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-welcome',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomePage implements OnInit {
 
+  @ViewChild('slider')  slider: IonSlides; 
+
+  slideOpts = {
+    initialSlide: 0
+  };
+
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
